4ef8c1f4c311b0145a95f466f09c81c777f829e7
wiwgit <202403391635@alunos.estacio.br>
2024-08-30 20:59:36 -0300

    Add files via upload

---
+++ circulo.py
@@ -0,0 +1,6 @@
+import math
+r = float(input("Digite o raio: "))
+
+area = math.pi * 5
+(r**2)
+print(f"A área do seu circulo é: {area:.5f}")
---
+++ exercicio01.py
@@ -0,0 +1,4 @@
+nota01 = float(input("Digite a nota 01: "))
+nota02 = float(input("Digite a nota 02: "))
+media= float(nota01 + nota02)/2
+print(f"A média entre {nota01} e {nota02} é: {media:.2f}")
